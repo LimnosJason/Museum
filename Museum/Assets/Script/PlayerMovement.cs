e796ad9109fd937a7552ed3f0c4d0b763e834d11
SofiaBili <sofia.bili@yahoo.gr>
2022-05-14 00:05:33 +0300

    elevator char control

--- Museum/Assets/Script/PlayerMovement.cs
+++ Museum/Assets/Script/PlayerMovement.cs
@@ -5,8 +5,18 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
     Rigidbody rb;
+    Vector3 velocity;
     [SerializeField] float movementSpeed = 8f;
     public CharacterController controller;
+
+    public float gravity = -9.81f;
+
+    public Transform groundCheck;
+    public float groundDistance = .1f;
+    public LayerMask groundMask;
+
+    bool isGrounded;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -19,12 +29,19 @@ public class PlayerMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask);
+        
+        if(isGrounded && velocity.y <0){
+            velocity.y = 0f;
+        }
         float horizontalInput = Input.GetAxis("Horizontal");
         float verticalInput = Input.GetAxis("Vertical");
         
         Vector3 move = transform.right * horizontalInput + transform.forward * verticalInput ;
         controller.Move(move*movementSpeed*Time.deltaTime);
-
+        
+        velocity.y += gravity * Time.deltaTime;
+        controller.Move(velocity * Time.deltaTime);
     }
 
 }
