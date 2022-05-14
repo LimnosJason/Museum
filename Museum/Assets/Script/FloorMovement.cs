89a56b453d65b27998a880dafef89a78b5e3605a
LimnosJason <limnosjason@gmail.com>
2022-05-14 13:23:19 +0300

    Complete Elevator

--- Museum/Assets/Script/FloorMovement.cs
+++ Museum/Assets/Script/FloorMovement.cs
@@ -4,27 +4,35 @@ using UnityEngine;
 
 public class FloorMovement : MonoBehaviour
 {
-    OpenDoor openDoor;
-    [SerializeField] GameObject DoorCheck;
+    OpenDoor openDoorDown;
+    OpenDoor openDoorUp;
+    [SerializeField] GameObject DoorCheckDown;
+    [SerializeField] GameObject DoorCheckUp;
     [SerializeField] Transform Floor;
+    [SerializeField] Transform Roof;
     [SerializeField] float speed = 1f;
     [SerializeField] GameObject[] FloorWaypoints;
+    [SerializeField] GameObject[] RoofWaypoints;
 
     int pos = 0;
     void Awake(){
-        openDoor = DoorCheck.GetComponent<OpenDoor>();
+        openDoorDown = DoorCheckDown.GetComponent<OpenDoor>();
+        openDoorUp = DoorCheckUp.GetComponent<OpenDoor>();
     }
-    
-    private void OnTriggerStay(Collider other){
-        if(openDoor.closedDoor){
-            if(Vector3.Distance(Floor.position, FloorWaypoints[pos].transform.position) < .1f){
-                pos++;
-                if(pos >= FloorWaypoints.Length){
-                    pos=0;
-                }
+    private void OnTriggerEnter(Collider other){
+        if(Vector3.Distance(Floor.position, FloorWaypoints[pos].transform.position) < .1f){
+            pos++;
+            if(pos >= FloorWaypoints.Length){
+                pos=0;
             }
-            
-            Floor.position = Vector3.MoveTowards(Floor.position, FloorWaypoints[pos].transform.position, speed*Time.deltaTime);
+        }
+    }
+    private void OnTriggerStay(Collider other){
+        if(openDoorDown.closedDoor && openDoorUp.closedDoor){
+            if(Vector3.Distance(Floor.position, FloorWaypoints[pos].transform.position) > .1f){
+                Floor.position = Vector3.MoveTowards(Floor.position, FloorWaypoints[pos].transform.position, speed*Time.deltaTime);
+                Roof.position = Vector3.MoveTowards(Roof.position, RoofWaypoints[pos].transform.position, speed*Time.deltaTime);
+            }
         }
     }
 
