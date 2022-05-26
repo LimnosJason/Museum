2d89bf283f46151e7f8272117f08513cd1f0d9e6
SofiaBili <sofia.bili@yahoo.gr>
2022-05-26 23:14:48 +0300

    add more

--- Museum/Assets/Script/BuyItem.cs
+++ Museum/Assets/Script/BuyItem.cs
@@ -3,28 +3,27 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using TMPro;
-public class ShoppingCart : MonoBehaviour
+public class BuyItem : MonoBehaviour
 {
-    [SerializeField] GameObject _canvas;
-    [SerializeField] GameObject _canvasCart;
+    
     [SerializeField] GameObject _plusButton;
     [SerializeField] GameObject _minusButton;
     [SerializeField] TMP_InputField  _itemCount;
-
-    public void CancelOnClick()
-    {
-        _canvas.SetActive(true);
-        _canvasCart.SetActive(false);
-    }
 
     public void PlusOnClick()
     {
         //var input = gameObject.GetComponent<InputField>();
         //Debug.Log(input.text);
         int numberOfItems =  int.Parse(_itemCount.GetComponent<TMP_InputField>().text);
-        Debug.Log(numberOfItems);
-         numberOfItems+=1;
-        Debug.Log(numberOfItems);
+        numberOfItems+=1;
+        _itemCount.GetComponent<TMP_InputField>().text=numberOfItems.ToString();
+    }
+    public void MinusOnClick()
+    {
+        int numberOfItems =  int.Parse(_itemCount.GetComponent<TMP_InputField>().text);
+        if(numberOfItems>1){
+            numberOfItems-=1;
+        }
         _itemCount.GetComponent<TMP_InputField>().text=numberOfItems.ToString();
     }
 }
