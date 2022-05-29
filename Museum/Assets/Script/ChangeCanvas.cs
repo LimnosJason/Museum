4156415994e23a5ca634c130d990f643818d8d38
SofiaBili <sofia.bili@yahoo.gr>
2022-05-29 12:50:55 +0300

    freeze player when canvas on top

--- Museum/Assets/Script/ChangeCanvas.cs
+++ Museum/Assets/Script/ChangeCanvas.cs
@@ -8,20 +8,24 @@ public class ChangeCanvas : MonoBehaviour
     [SerializeField] GameObject _canvas;
     [SerializeField] GameObject _canvasNew;
     void Update(){
+        PauseGame();
         if(Input.GetKeyDown(KeyCode.Escape)){
-            DisableCanvas();
+            CancelOnClick();
         }
-    }       
+    }
+    void PauseGame(){
+        Time.timeScale = 0;
+    }      
+    void ResumeGame(){
+        Time.timeScale = 1;
+    }
     void Awake(){
-        DisableCanvas();
+        CancelOnClick();
     }
 
-    public void DisableCanvas(){
-        _canvasNew.SetActive(false);
-        _canvas.SetActive(true);
-    }
     public void CancelOnClick()
     {
+        ResumeGame();
         _canvas.SetActive(true);
         _canvasNew.SetActive(false);
     }
